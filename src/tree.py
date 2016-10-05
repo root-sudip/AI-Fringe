ef38b8f75184f69a9b5f13046270361b29f75fed
root-sudip <touch.das@gmail.com>
2016-10-06 02:14:37 +0530

    init

---
+++ src/tree.py
@@ -0,0 +1,15 @@
+import sys
+import os
+if __name__ == '__main__':
+	try:
+		class node:
+			def __init_(self):
+				self.data = None
+
+	except KeyboardInterrupt:
+		try:
+			print('\n')
+			sys.exit(0)
+		except SystemExit:
+			print('\n')
+			os._exit(0)
