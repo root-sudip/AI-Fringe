2a9553cdcfe779c5d8f354249f4acf67aa292432
root-sudip <touch.das@gmail.com>
2016-10-05 19:50:47 +0530

    init

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
 import os 
+import sys
 #strating project
+if __name__ == '__main__':
+	try:
+		path =  input('Enter path name : ')
+		for name in os.walk(path):
+			print(name)
+	except KeyboardInterrupt:
+		print(' \n Interrupted \n')
+		try:
+			sys.exit(0)
+		except SystemExit:
+			os._exit(0)
+
